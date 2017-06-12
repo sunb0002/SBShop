00548524afafdc8ab6e3e2933fa92e5cca33a562
sun.bo <st_sunbo@hotmail.com>
2017-06-12 23:57:44 +0800

    SBShop V0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,8 @@
+import { RoutesModule } from './routes/routes.module';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from './shared/shared.module';
+import { LayoutModule } from './layout/layout.module';
+import { AuthModule } from './auth/auth.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -12,7 +17,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AuthModule,
+    LayoutModule,
+    CommonModule,
+    SharedModule,
+    RoutesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
