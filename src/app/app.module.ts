a9c225c5741ea0690adba849548afdfff7ffa787
sun.bo <st_sunbo@hotmail.com>
2017-06-26 11:52:25 +0800

    Service shouldn't be placed in shared module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { RoutesModule } from './routes/routes.module';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from './shared/shared.module';
 import { LayoutModule } from './layout/layout.module';
-import { AuthModule } from './auth/auth.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from 'app/services/auth/auth.module';
 
 @NgModule({
   declarations: [
