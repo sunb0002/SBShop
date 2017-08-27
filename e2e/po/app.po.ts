b588622908e0d3ad0f4d4fedbadac44cc2244932
sun.bo <st_sunbo@hotmail.com>
2017-08-27 20:16:18 +0800

    Share this my-style Angular2 e2e. Please let me know if you have better idea. Thanks.

---
+++ e2e/po/app.po.ts
@@ -0,0 +1,180 @@
+import { browser, by, element, ExpectedConditions } from 'protractor';
+import { ElementArrayFinder, ElementFinder } from 'protractor/built/element';
+import * as _superagent from 'superagent';
+
+import { environment } from '../../src/environments/environment';
+
+export class SBSHOPLogisticsPage {
+
+  public SBSHOPPageBaseHref = '/#';
+  public SBSHOPApiBaseURL = environment.SBSHOP_API.BASE_PATH;
+  public defaultSBSHOPUser = 'S2222222D';
+  public defaultSBSHOPLogisticsGroup = 'ESERVICE-ADMIN-INTRANET';
+  public defaultSBSHOPStoreGroup = 'PORTAL-SCDF_NSFS-INTERNET';
+  public defaultSBSHOPLoginType = 'SINGPASS';
+
+  defaultVisibilityTimeout = 3000;
+  defaultVisibilityTimeoutMsg = 'Taking too long to display (or hide) element.';
+  EC = ExpectedConditions;
+
+  defaultHttpRequestTimeout = 24000;
+
+  navigateToPath(path?: any) {
+    if (!path) {
+      path = '/';
+    }
+    browser.get(path);
+    browser.waitForAngular();
+  }
+
+  navigateToSBSHOPPath(path?: any) {
+    if (!path) {
+      path = '/';
+    }
+    if (path.indexOf(this.SBSHOPPageBaseHref) !== 0) {
+      this.navigateToPath(this.SBSHOPPageBaseHref + path);
+    } else {
+      this.navigateToPath(path);
+    }
+  }
+
+  public setSBSHOPTestingCookie(ivUser?: string, ivGroup?: string, loginType?: string): Promise<any> {
+
+    ivUser = ivUser || this.defaultSBSHOPUser;
+    ivGroup = ivGroup || this.defaultSBSHOPLogisticsGroup;
+    loginType = loginType || this.defaultSBSHOPLoginType;
+
+    // Unable to set cross-domain cookie. Have to redirect and then set.
+    browser.waitForAngularEnabled(false);
+    this.navigateToPath(this.SBSHOPApiBaseURL);
+    browser.manage().deleteAllCookies();
+    return Promise.all([
+      this.setCookie('iv-user', ivUser),
+      this.setCookie('iv-groups', ivGroup),
+      this.setCookie('logintype', loginType)
+    ]);
+  }
+
+  /**
+   * @param name
+   * @param value
+   */
+  public setCookie(name: string, value: string): Promise<void> {
+    // Protractor bug: browser.manage() TS definition is never updated. Have to cast as "any".
+    return (browser.manage() as any).addCookie({ 'name': name, 'value': value });
+  }
+
+  getElementByCss(cssSelector: string) {
+    return element(by.css(cssSelector));
+  }
+
+  getAllElementsByCss(cssSelector: string) {
+    return element.all(by.css(cssSelector));
+  }
+
+  getFilterButton() {
+    return element(by.buttonText('Filter'));
+  }
+
+  getSearchButton() {
+    return element(by.buttonText('Search'));
+  }
+
+  getBackButton() {
+    return element(by.buttonText('Back'));
+  }
+
+  getConfirmButton() {
+    return element(by.buttonText('Confirm'));
+  }
+
+  getContinueButton() {
+    return element(by.buttonText('Continue'));
+  }
+
+  /**
+   * Custom element visibility lock. Only needed before click/sendKey actions.
+   * @param {any} elm
+   * @param {number} [timeout]
+   * @param {string} [msg]
+   * @memberof SBSHOPStorePage
+   */
+  async awaitVisibility(elm, timeout?: number, msg?: string): Promise<void> {
+    await browser.wait(this.EC.visibilityOf(elm),
+      timeout || this.defaultVisibilityTimeout,
+      msg || this.defaultVisibilityTimeoutMsg);
+  }
+
+  // This only works for BootStrap3/4 Modal shade..
+  async awaitBootStrapModalVisibility(modalOn: boolean): Promise<void> {
+    const modalFadeAnimation = this.getElementByCss('.modal-backdrop.fade');
+    return modalOn ? await this.awaitVisibility(modalFadeAnimation) : await this.awaitInVisibility(modalFadeAnimation);
+  }
+
+  async awaitInVisibility(elm, timeout?: number, msg?: string): Promise<void> {
+    await browser.wait(this.EC.invisibilityOf(elm),
+      timeout || this.defaultVisibilityTimeout,
+      msg || this.defaultVisibilityTimeoutMsg);
+  }
+
+  async scrollToBottom(): Promise<void> {
+    await browser.executeScript('window.scrollTo(0,10000);');
+  }
+
+  async countElementsByTraversePaginations(elmArray: ElementArrayFinder, nextBtn: ElementFinder): Promise<number> {
+
+    let sum = 0;
+    let nextBtnDisabled: Boolean = false;
+    do {
+      await this.awaitBootStrapModalVisibility(false);
+      await this.awaitVisibility(nextBtn);
+      sum += await elmArray.count();
+      nextBtnDisabled = (await nextBtn.getAttribute('disabled') != null); // .isEnabled() is not working
+      if (!nextBtnDisabled) {
+        await nextBtn.click();
+        browser.waitForAngular();
+      }
+    } while (!nextBtnDisabled);
+
+    return sum;
+  }
+
+  async simpleSBSHOPPOST(path: string, postbody: any, ivUser?: string, ivGroup?: string, loginType?: string): Promise<any> {
+
+    const endpoint = this.SBSHOPApiBaseURL + path;
+    ivUser = ivUser || this.defaultSBSHOPUser;
+    ivGroup = ivGroup || this.defaultSBSHOPStoreGroup;
+    loginType = loginType || this.defaultSBSHOPLoginType;
+
+    const cookiestr = 'iv-groups=' + ivGroup + ';iv-user=' + ivUser + ';logintype=' + loginType;
+    const respbody = await this.simpleRestfulPOST(endpoint, postbody, cookiestr);
+    return (!respbody && respbody.status > 300) ? null : respbody.data;
+  }
+
+
+  /**
+   *
+   * @param {string} url
+   * @param {*} postbody
+   * @param {string} [cookiestr]
+   * @returns {Promise<any>}
+   * @memberof SBSHOPLogisticsPage
+   */
+  async simpleRestfulPOST(url: string, postbody: any, cookiestr?: string): Promise<any> {
+
+    console.log('simpleRestfulPOST with body', url, postbody);
+    return _superagent.post(url)
+      .set('Content-Type', 'application/json;charset=UTF-8')
+      .set('Cookie', cookiestr || '')
+      .send(postbody)
+      .timeout(this.defaultHttpRequestTimeout)
+      .then(
+      resp => resp.body,
+      err => {
+        console.log('Request Failed: ', err);
+        return null;
+      }
+      );
+  }
+
+}
