0cb8fd0c26ff88731f94609697b2d1a7f6010602
sun.bo <st_sunbo@hotmail.com>
2018-01-31 02:00:37 +0800

    new page/components

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CarouselComponent } from 'app/layout/mainbody/carousel/carousel.component';
 import { RoutesRoutingModule } from 'app/routes/routes-routing.module';
 
-import { CarouselComponent } from './carousel/carousel.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { ErrorComponent } from './mainbody/error/error.component';
+import { ProfileComponent } from './mainbody/profile/profile.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
@@ -12,7 +14,7 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     CommonModule,
     RoutesRoutingModule
   ],
-  declarations: [HeaderComponent, SidebarComponent, FooterComponent, CarouselComponent],
-  exports: [RoutesRoutingModule, HeaderComponent, SidebarComponent, FooterComponent, CarouselComponent]
+  declarations: [HeaderComponent, SidebarComponent, FooterComponent, CarouselComponent, ProfileComponent, ErrorComponent],
+  exports: [RoutesRoutingModule, HeaderComponent, SidebarComponent, FooterComponent, CarouselComponent, ProfileComponent, ErrorComponent]
 })
 export class LayoutModule { }
--- src/app/routes/routes-routing.module.ts
+++ src/app/routes/routes-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CarouselComponent } from 'app/layout/carousel/carousel.component';
+import { CarouselComponent } from 'app/layout/mainbody/carousel/carousel.component';
+import { ErrorComponent } from 'app/layout/mainbody/error/error.component';
+import { ProfileComponent } from 'app/layout/mainbody/profile/profile.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
   { path: 'dashboard', pathMatch: 'full', component: CarouselComponent },
+  { path: 'profile', pathMatch: 'full', component: ProfileComponent },
+  { path: 'error', pathMatch: 'full', component: ErrorComponent },
+  { path: '**', redirectTo: 'dashboard' },
 ];
 
 @NgModule({
